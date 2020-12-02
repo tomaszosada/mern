ed14cf74a7122e32e7456642c6ced39e2677ced3
Tomasz Osada <tomasz.osada98@gmail.com>
2020-12-02 14:31:49 +0100

    not sure what happened

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+//connect to database
+connectDB();
+
+//INIT middleware
+
+app.use(express.json({extended: false}));
 
 app.get("/", function(req,res){
     res.send("OK");
 })
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
